52c7fcd2fc3bb8178374a9ba6a5eef48ef04795e
JordanHolberton <jordan.chatillon76@laposte.net>
2024-11-11 15:38:07 +0100

    Refactor Classroom import and update Currency class validation messages

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,12 +1,12 @@
-/* Requirements */
-import Classroom from "./0-classroom";
+/*eslint-disable*/
+import ClassRoom from './0-classroom.js';
 
 function initializeRooms() {
-    return [
-        new Classroom(19),
-        new Classroom(20),
-        new Classroom(34),
-    ];
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34)
+  ];
 }
 
 export default initializeRooms;
--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,32 +1,33 @@
+/* eslint-disable */
 export default class Currency {
-  constructor(code, name) {
-    this._code = code;
-    this._name = name;
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(code) {
-    if (typeof code !== 'string') {
-      throw new TypeError('Code must be a String');
+    constructor(code, name) {
+        this._code = code;
+        this._name = name;
     }
-    this._code = code;
-  }
 
-  get name() {
-    return this._name;
-  }
-
-  set name(name) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a String');
+    get code() {
+        return this._code;
     }
-    this._name = name;
-  }
 
-  displayFullCurrency() {
-    return `${this._name} (${this._code})`;
-  }
+    set code(newCode) {
+        if (typeof newCode !== 'string') {
+            throw new TypeError('Code must be a string');
+        }
+        this._code = newCode;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(newName) {
+        if (typeof newName !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        this._name = newName;
+    }
+
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`;
+    }
 }
