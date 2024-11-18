c980c764e8193cebef8ad7f7c692400749b8de84
JordanHolberton <jordan.chatillon76@laposte.net>
2024-11-18 13:27:46 +0100

    Refactor handleProfileSignup to use arrow function and improve error message formatting

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,13 +1,9 @@
 /* eslint-disable */
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-        .then((results) => {
-            return results.map((result) => ({
-                status: result.status,
-                value: result.status === 'fulfilled' ? result.value : result.reason,
-            }));
-        });
-}
+export default (firstName, lastName, fileName) => Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  .then((results) => results.map((promise) => ({
+    status: promise.status,
+    value: promise.status === 'fulfilled' ? promise.value : `${promise.reason.name}: ${promise.reason.message}`,
+  })));
