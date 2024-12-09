72646bf768fc1449394d948ee42e04dccf1f7b36
JordanHolberton <jordan.chatillon76@laposte.net>
2024-12-09 10:52:56 +0100

    Add type annotation for async_generator function

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Generate 10 random numbers with a 1 second delay"""
     for _ in range(10):
         await asyncio.sleep(1)
