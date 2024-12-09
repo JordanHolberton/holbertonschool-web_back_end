32c8ce84d3f77f90f6b7f4868b4a8dfe3fde3c88
JordanHolberton <jordan.chatillon76@laposte.net>
2024-12-09 10:48:13 +0100

    Add async_generator function to generate random numbers with delay

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
+"""Async Generator"""
 
+import asyncio
+import random
+
+
+async def async_generator():
+    """Generate 10 random numbers with a 1 second delay"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
