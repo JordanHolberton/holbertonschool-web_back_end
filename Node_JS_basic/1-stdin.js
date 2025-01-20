e9d50049df2b183bc0149fca5665392703676e4e
JordanHolberton <jordan.chatillon76@laposte.net>
2025-01-20 09:50:33 +0100

    Fix input handling to ensure data is converted to string before trimming

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -7,7 +7,7 @@ console.log("Welcome to Holberton School, what is your name?");
 // Écoute les données d'entrée de stdin
 process.stdin.on('data', (data) => {
   // Supprime les espaces blancs en début et fin de chaîne
-  const input = data.trim();
+  const input = data.toString().trim();
   
   // Affiche le nom de l'utilisateur
   console.log('Your name is: ' + input);
