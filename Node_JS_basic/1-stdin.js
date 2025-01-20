fd72599c3b7760685c0069cf041ea808bd612862
JordanHolberton <jordan.chatillon76@laposte.net>
2025-01-20 10:02:42 +0100

    Refactor input handling and output formatting in stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,18 +2,18 @@
 process.stdin.setEncoding('utf8');
 
 // Affiche le message initial demandant le nom de l'utilisateur
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
 // Écoute les données d'entrée de stdin
 process.stdin.on('data', (data) => {
   // Supprime les espaces blancs en début et fin de chaîne
   const input = data.toString().trim();
-  
+
   // Affiche le nom de l'utilisateur
-  console.log('Your name is: ' + input);
-  
+  console.log(`Your name is: ${input}`);
+
   // Affiche le message de fermeture
   console.log('This important software is now closing');
-  
+
   // Supprime le processus.exit() pour permettre à l'événement 'end' d'être géré
 });
